bf1d91d3e629ae72cc39b025e2576010b31dd5e4
Aswin Kumar <achu4798@gmail.com>
2024-09-08 20:34:10 -0400

    Update Firebase.js

--- app/Firebase.js
+++ app/Firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAJSlylMfDIgED1lCOEMoZwU68Mfar2aNM",
+  authDomain: "fit-fusion-934c5.firebaseapp.com",
+  projectId: "fit-fusion-934c5",
+  storageBucket: "fit-fusion-934c5.appspot.com",
+  messagingSenderId: "444551416447",
+  appId: "1:444551416447:web:d0db4cc58e184022c40d4a",
+  measurementId: "G-FT5GTSYWXV",
 };
 
 // Initialize Firebase
